42e19cc905fb1cb4d1f27863dc96fecdbd42e406
Cheyenne Goh <cheyennegoh@Cheyennes-MacBook-Pro.local>
2021-12-05 10:16:40 -0700

    .

--- src/User_Interface_Layer/LoginForm.java
+++ src/User_Interface_Layer/LoginForm.java
@@ -1,3 +1,12 @@
+/*
+ * File Name: LoginForm.java
+ * Authors:
+ *      Cheyenne Goh (UCID: 30040528)
+ *      Divyansh Goyal (UCID: 30089488)
+ *      Rui Guan (UCID: 30072848)
+ *      Sajid Hafiz (UCID: 30061336)
+ */
+
 package User_Interface_Layer;
 // import LoginController.*;
 import javax.swing.*;
--- src/User_Interface_Layer/StartUp.java
+++ src/User_Interface_Layer/StartUp.java
@@ -1,3 +1,12 @@
+/*
+ * File Name: StartUp.java
+ * Authors:
+ *      Cheyenne Goh (UCID: 30040528)
+ *      Divyansh Goyal (UCID: 30089488)
+ *      Rui Guan (UCID: 30072848)
+ *      Sajid Hafiz (UCID: 30061336)
+ */
+
 package User_Interface_Layer;
 import javax.swing.*;
 import java.awt.*;
